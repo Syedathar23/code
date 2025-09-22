fe0e651a5b0e51ae3ffddddee6142021adf19660
syed Athar <150650323+Syedathar23@users.noreply.github.com>
2025-09-22 16:53:19 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const BACKEND_URL = "https://your-backend.onrender.com"; // Replace with your Render backend URL
+const BACKEND_URL = "https://code-tdwd.onrender.com"; // Replace with your Render backend URL
 export default BACKEND_URL;
